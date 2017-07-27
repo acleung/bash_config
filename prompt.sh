26693a5dc29daca81cdb6028967436007641a0f4
Alan Leung <acleung@google.com>
2017-07-26 20:00:48 -0700

    More coloring

--- prompt.sh
+++ prompt.sh
@@ -16,6 +16,18 @@ function __getgitbranch {
  fi
 }
 
+function __getpwd {
+ local WHITE="\001\e[00;00m\002"
+ local GRAY="\001\e[00;90m\002"
+ local COLORPWD=" ${WHITE}"
+ for d in `dirs +0 | tr "\/" "\n"`; do
+  COLORPWD="${COLORPWD}${d}${GRAY}/${WHITE}"
+ done
+ echo -e "$COLORPWD"
+# local COLORPWD=`dirs +0 | sed -e 's/\//\\e[00;31mhi\/${WHITE}/'`
+ #echo -e "${WHITE}$COLORPWD"
+}
+
 function __setprompt
 {
   local  WHITE="\[\033[00;00m\]"
@@ -23,36 +35,22 @@ function __setprompt
   local  GREEN="\[\033[00;32m\]"
   local   BLUE="\[\033[01;34m\]"
   local   CYAN="\[\033[00;36m\]"
-  local   GRAY="\[\033[00;37m\]"
+  local   GRAY="\[\033[00;90m\]"
   local YELLOW="\[\033[1;33m\]"
   local hn=`hostname -a | tr -d ' '`
   local me=`whoami`
 
   # LINE 1 [ PWD ]
-  PS1="${BLUE}[ ${GRAY}\w"
+  PS1="${BLUE}["
   PS1="$PS1${RED}\`RET=\$?; if [ \$RET != 0 ] ; then echo \" (\$RET)\"; fi\`"
+  PS1="$PS1\$(__getpwd)"
+  PS1="$PS1\$(__getgitbranch)"
   PS1="$PS1${BLUE} ]"
   PS1="$PS1\n"
   # LINE 2 
   # First get the current git branch
-  PS1="$PS1${BLUE}[\$(__getgitbranch)"
-  PS1="$PS1 ${WHITE}${me}${WHITE}@${WHITE}${hn} ${BLUE}]${WHITE}: "
+  PS1="$PS1 ${WHITE}${me}${GRAY}@${WHITE}${hn}${GRAY}:"
+  PS1="$PS1 ${WHITE}"
   PS2=">>>> "
 }
 __setprompt
-
-function __setprompt_oneline {
-  local  WHITE="\[\033[00;00m\]"
-  local    RED="\[\033[00;31m\]"
-  local  GREEN="\[\033[00;32m\]"
-  local   BLUE="\[\033[01;34m\]"
-  local   CYAN="\[\033[00;36m\]"
-  local   GRAY="\[\033[00;37m\]"
-  local YELLOW="\[\033[1;33m\]"
-  local hn=`hostname -a | tr -d ' '`
-  local me=`whoami`
-
-  # LINE 1 [ PWD ]
-  PS1="${BLUE}[ ${WHITE}${me}${WHITE}@${WHITE}${hn} ${BLUE}]${WHITE}: "
-  PS2=">>>> "
-}
